3f564a08c4c02a94bab1e08b93d64821aacd4a4e
Sus4nne <victorisu@gmail.com>
2020-09-16 19:23:05 +0200

    Try https enabled with keystore

--- src/test/java/nl/innovate/databaseAPI/DatabaseApiApplicationTests.java
+++ src/test/java/nl/innovate/databaseAPI/DatabaseApiApplicationTests.java
@@ -1,13 +1,34 @@
 package nl.innovate.databaseAPI;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext
 class DatabaseApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testHome() throws Exception {
+		ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(entity.getBody()).isEqualTo("Welkom op deze pagina.");
 	}
 
 }
